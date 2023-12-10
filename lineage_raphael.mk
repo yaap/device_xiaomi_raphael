f854f2fd8b8ee6595e6aa7dd68567ea3ff2419dc
kondors1995 <normandija1945@gmail.com>
2023-12-10 10:36:06 +0000

    raphael:Inital adaptation to CR 14.0

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -12,17 +12,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Suberia OS stuff.
-$(call inherit-product, vendor/syberia/common.mk)
-SYBERIA_BUILD_TYPE := OFFICIAL
-EXTRA_UDFPS_ANIMATIONS := true
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Matrixx
+MATRIXX_BUILD_TYPE := Personal
+MATRIXX_MAINTAINER := NATO66613
+MATRIXX_CHIPSET := SM8150
+MATRIXX_BATTERY := 4000mAh
+MATRIXX_DISPLAY := 1080x2400
+WITH_GAPPS := true
+TARGET_HAS_UDFPS := true
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_PIXEL_CHARGER := true
-TARGET_SUPPORTS_QUICK_TAP := true
-WITH_GMS := true
+TARGET_ENABLE_BLUR := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := syberia_raphael
+PRODUCT_NAME := lineage_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
