3d526e5a793193df8a7d1fd82ae15e1a48683001
kondors1995 <normandija1945@gmail.com>
2024-03-27 07:13:15 +0200

    raphael: Add default fingerprint
    As of Android 14 QPR2, raphael's release key fingerprints are longer than 92 characters.
    This causes crashes on prop read. So, hardcode a default that is shorter than 92 characters, so it can be overridden later.

--- yaap_raphael.mk
+++ yaap_raphael.mk
@@ -35,3 +35,5 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := Xiaomi/raphael/raphael:11/RKQ1.200826.002/V12.5.2.0.RFKMIXM:user/release-keys
