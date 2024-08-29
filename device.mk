a543c4cd35254567b0c0bf852af90b1f96418728
kondors1995 <normandija1945@gmail.com>
2024-08-29 09:14:49 +0300

    raphael: Import cleaned up coral powerstats
    *Removed pixel specifc bits
    * Updated wlan node

--- device.mk
+++ device.mk
@@ -458,6 +458,10 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# Powerstats
+PRODUCT_PACKAGES += \
+    android.hardware.power.stats@1.0-service.raphael
+
 # Perf
 PRODUCT_PACKAGES += \
     libqti-perfd-client
