dda6fc57167318c0753bd89affa874733ade918f
kondors1995 <normandija1945@gmail.com>
2026-04-15 20:32:11 +0000

    raphael: Update task profiles
    This fixes following warning, with is caused by intial shiping api being 28
    
    03-01 17:44:09.991 W/libprocessgroup(2209): SetCgroup::ExecuteForProcess: failed to open /dev/stune/background/cgroup.procs: No such file or directory

--- device.mk
+++ device.mk
@@ -570,7 +570,7 @@ PRODUCT_PACKAGES += \
 
 # Task Profiles
 PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    $(LOCAL_PATH)/configs/etc/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
 
 # Tetheroffload
