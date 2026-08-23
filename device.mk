2e12ab0d1283ec36d3d3397ccb18a202a2133373
kondors1995 <normandija1945@gmail.com>
2026-08-23 19:05:20 +0000

    raphael: use hardware/google/pixel/power-libperfmgr

--- device.mk
+++ device.mk
@@ -48,7 +48,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     bootable/deprecated-ota \
     hardware/google/interfaces \
-    hardware/google/pixel \
+    hardware/google/pixel/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
